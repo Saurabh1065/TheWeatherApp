946de0cc082775adc9ddb71e62507d56c1cb23e5
Saurabh <saurabh1065@gmail.com>
2017-06-30 23:00:10 +0530

    inflator used in mainActivity

--- app/src/main/java/com/apkglobal/theweatherapp/MainActivity.java
+++ app/src/main/java/com/apkglobal/theweatherapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.apkglobal.theweatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
